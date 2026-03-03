bea019d0aa9a883ea48396ec20834cd2561a8934
Sneh Sindhi <snehsindhi2004@gmail.com>
2026-03-03 12:12:03 +0530

    first commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Dummy from './Dummy'
+
+    //rfc-react function component
+    //rafce- react arrow function component with export
+
+    const App = () => {
+      return (
+        <div>
+            <h1>App component</h1>
+            <hr />
+            <Dummy />
+        </div>
+      )
+    }
+    
+    export default App
---
+++ my-first-react-app/src/Dummy.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const 
+Dummy = () => {
+  return (
+    <div>
+        <h1>Dummy Component</h1>
+        <hr />
+    </div>
+  )
+}
+
+export default Dummy;
